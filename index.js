6b28bc7b2bef9348c6fd3eb0b2972ea4615932c9
Mahmoud hammad <mahmoud.hammad.ali@gmail.com>
2026-03-23 21:31:42 +0200

    setup the database connection

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+require('dotenv').config();
+const pool = require('./db/dbConnectoin');
+
 const app = express();
+
+pool
+  .query('SELECT NOW()')
+  .then(res => console.log('✅ DB Connected:', res.rows[0]))
+  .catch(err => console.error('❌ Error:', err.message));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 3000, () => console.log('Server running'));
